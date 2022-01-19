a99f882749e02b4930fd4fa10e44e4152e7a71eb
Leonardo <tteixeira.leonardo@gmail.com>
2022-01-19 14:09:10 -0300

    build(boilerplate): cria o boilerplate para aplicacao reactjs
    instala as dependencias necessarias para comecar a desenvolver uma aplicacao reactjs
    
    fix #1

---
+++ webpack.dev.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const common = require('./webpack.common');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const { merge } = require('webpack-merge');
+
+module.exports = merge(common, {
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    hot: true,
+    port: 9000,
+    open: true,
+  },
+  plugins: [new ReactRefreshWebpackPlugin()].filter(Boolean),
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [require.resolve('react-refresh/babel')].filter(Boolean),
+          },
+        },
+      },
+    ],
+  },
+});
